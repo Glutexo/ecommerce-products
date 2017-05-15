73d29aef762092b43ac57dddca18891e1dcbdd39
Štěpán Tomsa <stepantomsa@icloud.com>
2017-05-15 08:02:44 +0200

    Change sample script strings to English.
    As the app is expected to be used by English-speaking customers, the
    strings in the sample script adjusted to be in their language.

--- bin/sample.rb
+++ bin/sample.rb
@@ -7,7 +7,7 @@ products << Product.new('MacBook Pro', '55990.00')
 products << Product.new('iMac', '39990.00')
 products << Product.new('Mac Pro', '95990.00')
 
-puts 'Máme na skladě:'
+puts 'On stock:'
 products.each do |product|
   puts "• #{product}"
 end
