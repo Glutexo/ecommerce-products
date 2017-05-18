466d8b11c1aecfc05233335b20e1b5e774418586
Štěpán Tomsa <stepantomsa@icloud.com>
2017-05-18 21:02:17 +0200

    Refactor tests not to repeat test products.
    All tests amended to use a pre-defined bundle of example products. These
    are named to easily find the extremes and at the same time easily
    convertible to a simple list to be passed to Cart and to perform
    computations on.

--- test/cart_test.rb
+++ test/cart_test.rb
@@ -3,45 +3,37 @@ require 'test/unit'
 
 class CartTest < Test::Unit::TestCase
 
+  PRODUCTS = { halfway:   Product.new('MacBook Pro', '55990.99'),
+               expensive: Product.new('Mac Pro', '95990.90'),
+               cheap:     Product.new('iMac', '39990.00') }
+
+
   def test_cart_has_products
-    products = [
-        Product.new('MacBook Pro', '55990.00'),
-        Product.new('iMac', '39990.00')
-    ]
-    cart = Cart.new products
-    assert_equal products, cart.products
+    product_list = PRODUCTS.values
+    cart = Cart.new product_list
+    assert_equal product_list, cart.products
   end
 
   def test_cart_computes_its_sum
-    prices = big_decimals ['55990.00', '39990.00']
-    products = products prices
-
-    cart = Cart.new products
-    assert_equal prices.sum, cart.sum
+    product_list = PRODUCTS.values
+    cart = Cart.new product_list
+    assert_equal sum(product_list), cart.sum
   end
 
   def test_cart_prints_its_sum
-    prices = big_decimals ['55990.00', '39990.00']
-    products = products prices
-
-    cart = Cart.new products
-    assert_equal prices.sum.to_s('F'), cart.to_s
+    product_list = PRODUCTS.values
+    cart = Cart.new product_list
+    assert_equal sum(product_list).to_s('F'), cart.to_s
   end
 
   def test_cart_finds_the_cheapest_product
-    products = { halfway:   Product.new('MacBook Pro', '55990.00'),
-                 expensive: Product.new('Mac Pro', '95990.00'),
-                 cheap:     Product.new('iMac', '39990.00') }
-    cart = Cart.new products.values
-    assert_same products[:cheap], cart.min
+    cart = Cart.new PRODUCTS.values
+    assert_same PRODUCTS[:cheap], cart.min
   end
 
   def test_cart_finds_the_most_expensive_product
-    products = { halfway:   Product.new('MacBook Pro', '55990.00'),
-                 expensive: Product.new('Mac Pro', '95990.00'),
-                 cheap:     Product.new('iMac', '39990.00') }
-    cart = Cart.new products.values
-    assert_same products[:expensive], cart.max
+    cart = Cart.new PRODUCTS.values
+    assert_same PRODUCTS[:expensive], cart.max
   end
 
   private
@@ -49,8 +41,8 @@ class CartTest < Test::Unit::TestCase
       strings.map &BigDecimal.method(:new)
     end
 
-    def products prices
-      prices.map do |price| Product.new 'Product', price end
+    def sum products
+      products.sum &:price
     end
 
 end
